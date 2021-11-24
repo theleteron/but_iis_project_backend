a4f20d9dd5a4d490d4bd770ba4c1e947e88c22ca
beagl-fit <72127299+beagl-fit@users.noreply.github.com>
2021-11-24 12:21:11 +0100

    added model serializers; check Bookloan + bookloanSerializer

--- backend/api_app/serializers.py
+++ backend/api_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Library
+from .models import Library, Publication, Book, PublicationOrder, BookOrder, BookLoan
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
 
@@ -17,6 +17,77 @@ class LibrarySerializer(serializers.ModelSerializer):
         model = Library
         fields = ('__all__')
 
+# Publication Serializer
+class PublicationSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=50)
+    series = serializers.CharField(max_length=50)
+    synopsis = serializers.CharField(max_length=255)
+    authors = serializers.CharField(max_length=255)
+    language = serializers.CharField(max_length=50)
+    ISBN = serializers.CharField(max_length=20, unique=True)
+    date_of_publication = serializers.DateTimeField()
+    publisher = serializers.CharField(max_length=50)
+    genre = serializers.CharField(max_length=50)
+    pages = serializers.IntegerField()
+    tags = serializers.CharField(max_length=255)
+    rating = serializers.FloatField()
+
+    class Meta:
+        model = Publication
+        fields = ('__all__')
+
+# Book Serializer
+class BookSerializer(serializers.ModelSerializer):
+    STATES = (
+        (1, "New"),
+        (2, "Used"),
+        (3, "Damaged"),
+    )
+
+    publication = serializers.PrimaryKeyRelatedField()
+    library = serializers.PrimaryKeyRelatedField()
+    condition = serializers.CharField(max_length=30, choices=STATES, default=1)
+    section = serializers.IntegerField()
+    loaned = serializers.BooleanField(default=False)
+
+    class Meta:
+        model = Book
+        fields = ('__all__')
+
+# PublicationOrder Serializer
+class PublicationOrderSerializer(serializers.ModelSerializer):
+    publication         = serializers.PrimaryKeyRelatedField()
+    user                = serializers.PrimaryKeyRelatedField()
+    date_of_order       = serializers.DateTimeField(auto_now_add=True)
+    delivered           = serializers.BooleanField(default=False)
+    price               = serializers.FloatField()
+
+    class Meta:
+        model = PublicationOrder
+        fields = ()
+
+# BookOrder Serializer
+class BookOrderSerializer(serializers.ModelSerializer):
+    publication_order   = serializers.PrimaryKeyRelatedField()
+    number_of_books     = serializers.IntegerField()
+    price_per_book      = serializers.FloatField()
+
+    class Meta:
+        model = BookOrder
+        fields = ('__all__')
+
+# BookLoan Serializer
+class BookLoanSerializer(serializers.ModelSerializer):
+    user                = serializers.PrimaryKeyRelatedField()
+    date_from           = serializers.DateTimeField()
+    date_to             = serializers.DateTimeField()
+    extension_to        = serializers.DateTimeField()
+    fine                = serializers.FloatField(default=0)
+    books               = serializers.PrimaryKeyRelatedField(many=True)
+
+    class Meta:
+        model = BookLoan
+        fields = ('__all__')
 
 ##!!! Following are taken from https://studygyaan.com/django/django-rest-framework-tutorial-register-login-logout
 # User Serializer
